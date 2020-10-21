ba300b96d0958c01fcf7fdc8685f7432436afde7
Hiromu <tsongcun80@gmail.com>
2020-10-21 16:11:21 +0900

    y

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def register():
         c.execute("insert into user values(null,?,?)", (name,password))
         conn.commit()
         conn.close()
-        return redirect('/login')
+        return redirect('/n_login')
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -249,6 +249,241 @@ def logout():
     session.pop('user_id',None)
     # ログアウト後はログインページにリダイレクトさせる
     return redirect("/")
+
+
+
+
+# 新規の方のルーティング
+# 新規の方のルーティング
+
+
+
+
+
+@app.route("/n_login", methods=["GET", "POST"])
+def n_login():
+    if request.method == "GET":
+        if 'user_id' in session :
+            return redirect("/n_aaa")
+        else:
+            return render_template("n_log.html")
+    else:
+        name = request.form.get("name")
+        password = request.form.get("password")
+        conn = sqlite3.connect('hitoiki.db')
+        c = conn.cursor()
+        c.execute("select id from user where name = ? and password = ?", (name, password) )
+        user_id = c.fetchone()
+        conn.close()
+        print(type(user_id))
+        if user_id is None:
+            return render_template("n_log.html")
+        else:
+            session['user_id'] = user_id[0]
+            return redirect("/n_aaa")
+
+@app.route("/n_aaa")
+def n_pose():
+    return render_template("n_q1.html")
+
+
+
+@app.route('/n_one', methods=['POST'])
+def n_out_one():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,1,?,?)", (user_id, name, time))
+    conn.commit()
+    conn.close()
+    return render_template("n_q2.html")
+
+
+
+@app.route('/n_two', methods=['POST'])
+def n_out_two():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,2,?,?)", (user_id ,name, time))
+    conn.commit()
+    conn.close()
+    return render_template("n_q3.html")
+
+
+@app.route('/n_three', methods=['POST'])
+def n_out_three():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,3,?,?)", (user_id, name, time))
+    conn.commit()
+    conn.close()
+    return render_template("n_q4.html")
+
+
+@app.route('/n_four', methods=['POST'])
+def n_out_four():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,4,?,?)", (user_id, name, time))
+    conn.commit()
+    conn.close()
+    return render_template("n_q5.html")
+
+
+@app.route('/n_five', methods=['POST'])
+def n_out_five():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,5,?,?)", (user_id, name, time))
+    conn.commit()
+    conn.close()
+    return render_template("n_q6.html")
+
+
+@app.route('/n_six', methods=['POST'])
+def n_out_six():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,6,?,?)", (user_id, name, time))
+    conn.commit()
+    conn.close()
+    return render_template("n_q7.html")
+
+
+@app.route('/n_seven', methods=['POST'])
+def n_out_seven():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,7,?,?)", (user_id, name, time))
+    conn.commit()
+    conn.close()
+    return render_template("n_q8.html")
+
+
+@app.route('/n_eight', methods=['POST'])
+def n_out_eight():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,8,?,?)", (user_id, name, time))
+    conn.commit()
+    conn.close()
+    return render_template("n_q9.html")
+
+
+@app.route('/n_nine', methods=['POST'])
+def n_out_nine():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,9,?,?)", (user_id, name, time))
+    conn.commit()
+    conn.close()
+    return render_template("n_q10.html")
+
+
+@app.route('/n_ten', methods=['POST'])
+def n_out_ten():
+    user_id=session['user_id']
+    name = int(request.form.get("radio"))
+    print(name)
+    time = datetime.now().strftime('%Y/%m/%d')
+    conn = sqlite3.connect('hitoiki.db')
+    c = conn.cursor()
+    
+    c.execute("INSERT INTO question VALUES (null,?,10,?,?)", (user_id, name, time))
+    conn.commit()
+    conn.close()
+    return redirect("/n_dbtest")
+
+
+@app.route("/n_dbtest")
+def n_dbtest():
+    user_id=session['user_id']
+    #flask.dbに接続
+    conn = sqlite3.connect("hitoiki.db")
+    #中身が見れるように
+    c = conn.cursor()
+    #SQL文を実行
+    c.execute("select anser  from question where question_id = 1")
+    #取ってきたレコードを格納(user_infoに代入している)
+    n_list1 = c.fetchone()
+    print(n_list1)
+    c.execute("select anser  from question where question_id = 2")
+    n_list2 = c.fetchone()
+    print(n_list2)
+    c.execute("select anser  from question where question_id = 3")
+    n_list3 = c.fetchone()
+    c.execute("select anser  from question where question_id = 4")
+    n_list4 = c.fetchone()
+    c.execute("select anser  from question where question_id = 5")
+    n_list5 = c.fetchone()
+    c.execute("select anser  from question where question_id = 6")
+    n_list6 = c.fetchone()
+    c.execute("select anser  from question where question_id = 7")
+    n_list7 = c.fetchone()
+    c.execute("select anser  from question where question_id = 8")
+    n_list8 = c.fetchone()
+    c.execute("select anser  from question where question_id = 9")
+    n_list9 = c.fetchone()
+    c.execute("select anser  from question where question_id = 10")
+    n_list10 = c.fetchone()
+
+
+
+    c.execute("select date  from question where question_id = 1 order by id desc")
+    n_time = c.fetchone()
+    print(n_time)
+    # dbとの接続を切る
+    c.close()
+    # list1 = [(1,2), (1,3), (2,4)]
+    return render_template("n_result.html",n_list1=n_list1, n_list2=n_list2, n_list3=n_list3 ,n_list4=n_list4, n_list5=n_list5, n_list6=n_list6, n_list7=n_list7, n_list8=n_list8,n_list9=n_list9, n_list10=n_list10, n_time=n_time)
+
+
+
+
 
 
 if __name__ == "__main__":
